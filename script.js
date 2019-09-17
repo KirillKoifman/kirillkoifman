fbeceafb4b7fd31410d5937738ea4c8f7941514c
KirillKoifman <54882995+KirillKoifman@users.noreply.github.com>
2019-09-17 23:30:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let a=null;
+if (typeof a==undefined)
+alert("...");
+else if(isNaN(a))
+alert("...");
+else if(a==Infinity)
+alert("...");
+else if(a==null)
+alert("...");
+else
+alert("...");
+function check() {
+let n=document.getElementById("num").
+value;
+alert("...");
+}
+
